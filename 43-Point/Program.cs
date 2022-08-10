352a5a85c55371a8c546343144aea541f47802ab
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-08-11 01:49:23 +0300

    Point app added

--- 43-Point/Program.cs
+++ 43-Point/Program.cs
@@ -8,3 +8,18 @@ x * (k1 - k2) = b2 - b1
 x = (b2 - b1) / (k1 - k2)
 */
 
+Console.WriteLine("Вычисление координат точки пересечения через линейные уравнения");
+Console.WriteLine("y = k1 * x + b1 и y = k2 * x + b2");
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Линии пересекаются в точке с координатами x = {x}, y = {y}");

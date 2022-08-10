8b5bf7a84c6983c5d8a7746848f6f5912c7173b0
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-08-11 02:13:13 +0300

    More Zero app added

---
+++ 41-More-Zero/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+
+int numbersCount = 0;
+int positivesCount = 0;
+string input = string.Empty;
+
+Console.Write("Введите число, или \"q\" для окончания ввода: ");
+while ((input = Console.ReadLine()) != "q")
+{
+    if (!input.Equals("q"))
+    {
+        if (Convert.ToInt32(input) > 0)
+            positivesCount++;
+        numbersCount++;
+        Console.Write("Введите следующее число, или \"q\" для выхода: ");
+    }
+}
+
+Console.WriteLine($"Вы ввели чисел: {numbersCount}, из них {positivesCount} - больше нуля");
